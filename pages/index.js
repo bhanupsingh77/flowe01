5b972cbda443e4c38bffad61fb4427dbccb21338
Home <home@Homes-MacBook-Air.local>
2024-01-09 23:02:19 +0530

    minor fix

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import { Inter } from "@next/font/google";
+import Script from "next/script";
 import ProductPromotion from "../src/components/productPromotion";
 import ProductCarousel from "../src/components/productCarousel";
 import BrandLocation from "../src/components/brandLocation";
@@ -40,6 +41,30 @@ export default function Home({ allPostsData }) {
           type="image/png"
           sizes="16x16"
           href="/favicon-16x16.png"
+        />
+        <Script
+          id="hotjar analytics"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `(function (h, o, t, j, a, r) {
+            h.hj =
+              h.hj ||
+              function () {
+                (h.hj.q = h.hj.q || []).push(arguments);
+              };
+            h._hjSettings = { hjid: 3819010, hjsv: 6 };
+            a = o.getElementsByTagName("head")[0];
+            r = o.createElement("script");
+            r.async = 1;
+            r.src = t + h._hjSettings.hjid + j + h._hjSettings.hjsv;
+            a.appendChild(r);
+          })(
+            window,
+            document,
+            "https://static.hotjar.com/c/hotjar-",
+            ".js?sv="
+          )`,
+          }}
         />
       </Head>
       <main>

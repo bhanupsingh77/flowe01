6178bc4ab0c076c1efc1b1ac42dee4651047824d
Home <home@Homes-MacBook-Air.local>
2024-01-09 22:31:29 +0530

    minor fix

--- pages/_document.js
+++ pages/_document.js
@@ -1,36 +1,9 @@
 import { Html, Head, Main, NextScript } from "next/document";
-import Script from "next/script";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
-        {/* <!-- Hotjar Tracking Code for lkbombay --> */}
-        <Script
-          id="hotjar analytics"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `(function (h, o, t, j, a, r) {
-              h.hj =
-                h.hj ||
-                function () {
-                  (h.hj.q = h.hj.q || []).push(arguments);
-                };
-              h._hjSettings = { hjid: 3819010, hjsv: 6 };
-              a = o.getElementsByTagName("head")[0];
-              r = o.createElement("script");
-              r.async = 1;
-              r.src = t + h._hjSettings.hjid + j + h._hjSettings.hjsv;
-              a.appendChild(r);
-            })(
-              window,
-              document,
-              "https://static.hotjar.com/c/hotjar-",
-              ".js?sv="
-            )`,
-          }}
-        />
-      </Head>
+      <Head />
       <body style={{ margin: "0px" }}>
         <Main />
         <NextScript />
